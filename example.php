536eb46a5bf415f373208fdb6f2f7c027f973c48
ipsBruno <email@brunodasilva.com>
2014-03-14 07:33:13 -0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+
+
+          $wallet = "ipsBruno"; // label to address
+          
+          $addressbtc = PegarEndereço("Bitcoin", $wallet);
+          $addressltc = PegarEndereço("Litecoin", $wallet);
+          
+          
+          $valuebtc = ValorDisponivel("Bitcoin", $wallet); 
+          $valueltc = ValorDisponivel("Litecoin", $wallet); 
+          
+          EnviarQuantia("Bitcoin", "addresstosend", $valor)
+
+?>
